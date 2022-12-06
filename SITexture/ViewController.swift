9ca8ee7123ecc9e19202ce858ea3e57463702917
王卫亮 <wangweiliang@shein.com>
2022-12-06 11:27:55 +0800

    Initial Commit

---
+++ SITexture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SITexture
+//
+//  Created by 王卫亮 on 2022/12/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
